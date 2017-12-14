fd1c1ea1ab5030a4df076bc2dc795ed928980d8f
Ropaper <rogeriodominus@gmIL.com.br>
2017-12-14 20:46:52 -0200

    Prieiro commit

---
+++ Workspace/Farmacia/src/br/com/farmacia/ben/FornecedoresBen.java
@@ -0,0 +1,39 @@
+package br.com.farmacia.ben;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+import javax.faces.model.ListDataModel;
+
+import br.com.farmacia.DAO.fornecedoresDAO;
+import br.com.farmacia.domain.Fornecedores;
+
+@ManagedBean(name = "MBFornecedores")
+@ViewScoped
+public class FornecedoresBen {
+
+	private ListDataModel<Fornecedores> itens;
+
+	public ListDataModel<Fornecedores> getItens() {
+		return itens;
+	}
+
+	public void setItens(ListDataModel<Fornecedores> itens) {
+		this.itens = itens;
+	}
+	
+	@PostConstruct //Quando a pagina for construida ele executará o método abaixo, por conta do (@PostConstruct)
+	public void preparaPesquisa() {
+
+		try {
+			fornecedoresDAO fdao = new fornecedoresDAO();
+			ArrayList<Fornecedores> lista = fdao.listar();
+			itens = new ListDataModel<Fornecedores>(lista);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+}
